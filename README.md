33e68a7442583f8badd26e18e1943bb897586724
kingsconsult <kingsconsult001@gmail.com>
2020-12-02 21:01:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Livewire CRUD
+
+## This is a basic CRUD project using Laravel and Livewire
---
+++ resources/views/livewire/post/show.blade.php
@@ -0,0 +1,4 @@
+<div>
+    In work, do what you enjoy.
+    <p> Let me enjoy Laravel and Livewire</p>
+</div>
